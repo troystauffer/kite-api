572ca2204aad4a1c5d258bc2967096205c675a72
Troy Stauffer <troystauffer@gmail.com>
2015-09-19 19:33:51 -0500

    adding notification migration

---
+++ migrations/20150919193012-create-notification.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      zip: {
+        type: Sequelize.INTEGER
+      },
+      optimum: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Notifications');
+  }
+};
